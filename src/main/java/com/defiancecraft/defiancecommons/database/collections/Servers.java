37899825dc2261920ff1805c705b3311bddc25af
davejbax <davejbax@gmail.com>
2015-02-13 20:24:55 +0000

    Update getByName method in Servers to play nice with MongoExceptions.

--- src/main/java/com/defiancecraft/defiancecommons/database/collections/Servers.java
+++ src/main/java/com/defiancecraft/defiancecommons/database/collections/Servers.java
@@ -2,6 +2,7 @@ package com.defiancecraft.defiancecommons.database.collections;
 
 import com.defiancecraft.defiancecommons.database.documents.DBServer;
 import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
 
 public class Servers extends Collection {
 
@@ -9,8 +10,30 @@ public class Servers extends Collection {
 		return "servers";
 	}
 	
+	/**
+	 * Finds a DBServer based on query; uses this
+	 * rather than generics solution due to performance.
+	 * 
+	 * Speed tests which tested use of generics vs this
+	 * showed ~0.2s taken off the time to retrieve 5000
+	 * documents on a local MongoDB server. 
+	 * 
+	 * @param query Query to find object
+	 * @return DBServer or null
+	 */
+	public DBServer findOne(DBObject query) {
+		DBObject obj = getDBC().findOne(query);
+		return obj == null ? null : new DBServer(obj);
+	}
+	
+	/**
+	 * Gets a server by name
+	 * 
+	 * @param name Name of server
+	 * @return DBServer or null
+	 */
 	public DBServer getByName(String name) {
-		return new DBServer(getDBC().findOne(new BasicDBObject(DBServer.FIELD_NAME, name)));
+		return findOne(new BasicDBObject(DBServer.FIELD_NAME, name));
 	}
 
 }
