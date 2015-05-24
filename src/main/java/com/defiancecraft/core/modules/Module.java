4fbff87fc409c9becc8f33c2ccaaa3ff89a91d43
davejbax <davejbax@gmail.com>
2015-05-24 17:26:12 +0100

    Remove implementation from Module interface.

--- src/main/java/com/defiancecraft/core/modules/Module.java
+++ src/main/java/com/defiancecraft/core/modules/Module.java
@@ -1,18 +1,6 @@
 package com.defiancecraft.core.modules;
 
-import java.util.Map;
-
-import org.bukkit.Bukkit;
-
-import com.defiancecraft.core.DefianceCore;
 import com.defiancecraft.core.database.collections.Collection;
-import com.defiancecraft.core.util.FileUtils;
-import com.defiancecraft.core.util.GsonUtils;
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 /**
  * A module is defined as a plugin which utilizes DefianceCore
@@ -50,50 +38,15 @@ public interface Module {
 	 * @param clazz Class representing the config to load.
 	 * @return The configuration, or null if `clazz` was invalid.
 	 */
-	public default <T> T getConfig(Class<T> clazz) {
-		
-		ModuleConfig config = DefianceCore.getModuleConfig();
-		Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
-		
-		if (config.configs.containsKey(getCanonicalName())) {
-			
-			// Return parsed config, if it exists in config list.
-			Map<String, JsonElement> objMap = config.configs.get(getCanonicalName());
-			JsonObject obj = GsonUtils.toJsonObject(objMap);
-			
-			return gson.fromJson(obj, clazz);
-			
-		} else {
-			
-			// Put default config in configs as a JsonObject, and return.
-			try {
-				
-				T t = clazz.newInstance();
-				
-				// Convert to JSON string, then parse as JsonObject... then to
-				// a Map<String, JsonElement>... wat.
-				// This is to avoid having "members": { ... } in the config of
-				// a module; it should simply be key-value pairs, whereas JsonObjects
-				// add a "members" field, which is a Map<String, JsonElement>.
-				Map<String, JsonElement> obj = GsonUtils.toMap(new JsonParser()
-					.parse(gson.toJson(t))
-					.getAsJsonObject());
-				
-				config.configs.put(getCanonicalName(), obj);
-				config.save(FileUtils.getSharedConfig("modules.json"));
-				DefianceCore.reloadModuleConfig();
-				
-				return t;
-				
-			} catch (InstantiationException | IllegalAccessException e) {
-				
-				Bukkit.getLogger().warning(String.format("%s while trying to load config of module; message: %s", e.getClass().getSimpleName(), e.getMessage()));
-				return null;
-			}
-			
-			
-		}
-		
-	}
+	public <T> T getConfig(Class<T> clazz);
+
+	/**
+	 * Saves the configuration to the storage medium used by the
+	 * module. This is usually JSON.
+	 * 
+	 * @param config An instance of the configuration to save
+	 * @return Whether the config saved successfully.
+	 */
+	public <T> boolean saveConfig(T config);
 	
 }
